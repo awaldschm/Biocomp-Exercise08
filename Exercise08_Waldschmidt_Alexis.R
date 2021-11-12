db02d5b1a8904d1944dddf10f8d830f95fe30060
Alexis Waldschmidt <awaldschm@gmail.com>
2021-11-12 00:48:16 -0500

    Updated comments to make my code more clear

--- Exercise08_Waldschmidt_Alexis.R
+++ Exercise08_Waldschmidt_Alexis.R
@@ -17,13 +17,13 @@ setwd("~/Intro to Biocomputing/Lesson5/Biocomp-Exercise08")
 game <- read.table("UWvMSU_1-22-13.txt", header = T, stringsAsFactors = F, sep = "")
 
 ##create two vectors with the team's cumulative score and put it in a dataframe 
-##do an if else loop for each team
 #create empty vectors
 UW_totalscore <- 0
 UW_scorevtime <- numeric(nrow(game))
 MSU_totalscore <- 0
 MSU_scorevtime <- numeric(nrow(game))
 
+#make a for loop to fill in the two empty vectors with each team's cumulative score
 for(i in 1:nrow(game)){
   if(game$team[i] == "UW"){
     UW_totalscore <- UW_totalscore + game$score[i]
@@ -36,23 +36,8 @@ for(i in 1:nrow(game)){
   }
 } #end loop
 
-##create a vector that contains the quarter time information
-#create an empty vector
-#quarter <- numeric(nrow(game))
-
-#make an if then and for loop that contains the quarter time information
-#for(i in 1:nrow(game)){
-#  if(game$time[i] < 10){
-#    quarter[i] <- "1st"
-#  }else if(game$time[i] < 20 & game$time[i] > 10){
-#    quarter[i] <- "2nd"
-#  }else if(game$time[i] < 30 & game$time[i] > 20){
-#    quarter[i] <- "3rd"
-#  }else{
-#    quarter[i] <- "4th"
-#  }
-#}#end for loop
-
+#create a vector that contains the quarter time information.
+#   Because the quarters are separated by 10 mins, divide the game time by 10
 quarter <- game$time/10
 
 #create a dataframe that contains four columns: one for the quarter, one for the
@@ -67,7 +52,7 @@ game.df <- data.frame(quarter, game$time, UW_scorevtime, MSU_scorevtime)
 #create a plot that is quarter vs score
 plot(x = game.df$quarter, y = game.df$UW_scorevtime, type='l', col = "blue", xaxt = "n", xlab = "Quarter", ylab = "Points")
 lines(x= game.df$quarter, y= game.df$MSU_scorevtime, type = 'l', col = "black", xlab = "Quarter", ylab = "Points")
-axis(1, at = c(1, 2, 3, 4), labels = c("1st", "2nd", "3rd", "4th")) #to specify the x axis so it is in quarters
+axis(1, at = c(1, 2, 3, 4), labels = c("1st", "2nd", "3rd", "4th")) #to specify the x axis so it is in quarters as its unit of time
 
 
 ###Question 2
@@ -79,7 +64,11 @@ axis(1, at = c(1, 2, 3, 4), labels = c("1st", "2nd", "3rd", "4th")) #to specify 
 #   Hint: sample() is a function that allows for random selection from a vector
 #   containing a set of integers
 
-#get the computer to generate a random number between 1 and 100
+#create a function that generates a random number, allows for user input,
+#   enters a for loop that will limit the number of guesses the user can input 
+#   to 10, and has an if/else statement that will give feedback to guide the 
+#   user to the correct number as well as exit the game once the correct number 
+#   is found
 guess_my_number <- function(x){var <- numeric(length = 10) #we will never actually put data into this function
 random_num <- as.numeric(sample(1:100, size = 1))
 print("I'm thinking of a number 1-100...")
@@ -97,8 +86,8 @@ for(i in 1:10){
     print("Correct!")
     break
   }
-}
-}
+}#end for loop
+}#end function
 
-#now to play the game, run this:
+#now to play the game, run the function and answer the prompts at the console:
 guess_my_number()
